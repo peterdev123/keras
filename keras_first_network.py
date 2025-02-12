a16afb1be96fcc9b7e136d534d23b04fc677518a
peterdev123 <petersylvanv123@gmail.com>
2025-02-12 11:54:34 +0800

    first commit

---
+++ keras_first_network.py
@@ -0,0 +1,36 @@
+from numpy import loadtxt  
+from tensorflow.keras.models import Sequential   # type: ignore
+from tensorflow.keras.layers import Dense   # type: ignore
+
+# load the dataset  
+
+dataset = loadtxt('Keras Activity\pima-indians-diabetes.csv', delimiter=',')  
+
+# split into input (X) and output (y) variables  
+
+X = dataset[:,0:8]  
+y = dataset[:,8]  
+
+# define the keras model  
+
+model = Sequential()  
+model.add(Dense(12, input_shape=(8,), activation='relu'))  
+model.add(Dense(8, activation='relu'))  
+model.add(Dense(1, activation='sigmoid'))
+
+# compile the keras model  
+
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])  
+
+# fit the keras model on the dataset  
+
+model.fit(X, y, epochs=150, batch_size=10, verbose=0)  
+
+# make class predictions with the model  
+
+predictions = (model.predict(X) > 0.5).astype(int)  
+
+# summarize the first 5 cases  
+
+for i in range(5):  
+    print('%s => %d (expected %d)' % (X[i].tolist(), predictions[i], y[i]))  
